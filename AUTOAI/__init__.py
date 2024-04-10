3c344e2c50fdd15e2edbfacafb17cfa10208e771
LeoLim3092 <leo.lim@mirlab.org>
2024-04-10 17:51:54 +0800

    remove CVfold_all_clf_evaluation from evaluation.py. It is not used and is implimented in evaluation with test results.

--- AUTOAI/__init__.py
+++ AUTOAI/__init__.py
@@ -2,7 +2,7 @@
 
 from .Training import Training, run_train, MySequentialFeatureSelector
 from .Visualization import plot_ROC_cruve, plot_feature_selection, plot_confusion_matrix, plot_pecision_recall_curve
-from .Evaluation import single_clf_evaluation, CVfold_all_clf_evaluation, all_clf_evaluate
+from .Evaluation import single_clf_evaluation, all_clf_evaluate
 
 __all__ = ["Training", "run_train", "MySequentialFeatureSelector", "plot_ROC_cruve", "plot_feature_selection", "plot_confusion_matrix",
-            "single_clf_evaluation", "CVfold_all_clf_evaluation", "all_clf_evaluate", "plot_pecision_recall_curve"]
+            "single_clf_evaluation", "all_clf_evaluate", "plot_pecision_recall_curve"]
